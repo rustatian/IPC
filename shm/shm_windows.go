35e12f87dda3ce4f6177c997c1b08fa3fbdd8688
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-11-01 00:04:03 +0300

    Add windows shared memory

--- shm/shm_windows.go
+++ shm/shm_windows.go
@@ -20,18 +20,26 @@ type SharedMemorySegment struct {
 }
 
 // https://docs.microsoft.com/en-us/windows/win32/memory/creating-named-shared-memory
-func NewSharedMemory(key string, size uint) (*SharedMemorySegment, error) {
+// CreateSharedMemory used to create or open existing
+func CreateSharedMemory(key string, size uint) (SharedMemory, error) {
 	name, err := syscall.UTF16PtrFromString(key)
 	if err != nil {
 		return nil, err
 	}
 
+	// args are:
+	// syscall.InvalidHandle - use paging file
+	// nil - default security
+	// syscall.PAGE_READWRITE - read/write access
+	// 0 - maximum object size (high-order DWORD)
+	// uint32(size) - maximum object size (low-order DWORD)
+	// name - name of mapping object
 	hMapFile, err := syscall.CreateFileMapping(syscall.InvalidHandle, nil, syscall.PAGE_READWRITE, 0, uint32(size), name)
 	if err != nil {
 		return nil, os.NewSyscallError("CreateFileMapping", err)
 	}
 
-	pBuf, err := syscall.MapViewOfFile(hMapFile, syscall.FILE_MAP_READ|syscall.FILE_MAP_WRITE, 0, 0, uintptr(size))
+	pBuf, err := syscall.MapViewOfFile(hMapFile, syscall.FILE_MAP_WRITE, 0, 0, uintptr(size))
 	if err != nil {
 		return nil, os.NewSyscallError("MapViewOfFile", err)
 	}
@@ -41,17 +49,34 @@ func NewSharedMemory(key string, size uint) (*SharedMemorySegment, error) {
 		size:    size,
 		handler: &hMapFile,
 		data:    make([]byte, int(size), int(size)),
+		addr:    pBuf,
 	}
 
 	// construct slice from memory segment
 	sh := (*reflect.SliceHeader)(unsafe.Pointer(&pBuf))
 	sh.Len = int(size)
 	sh.Cap = int(size)
-	data := *(*[]byte)(unsafe.Pointer(&sh))
-	segment.data = data
+	sh.Data = pBuf
+
+	segment.data = *(*[]byte)(unsafe.Pointer(&sh))
 
 	return segment, nil
 }
+
+//func AttachToSharedMemorySegment(key string, size uint) (SharedMemory, error) {
+//	name, err := syscall.UTF16PtrFromString(key)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	syscall.createfi
+//
+//	shm := &SharedMemorySegment{}
+//
+//	os.OpenFile(key, )
+//
+//	return shm, nil
+//}
 
 func (shm *SharedMemorySegment) Write(data []byte) {
 	for i := 0; i < len(data); i++ {
